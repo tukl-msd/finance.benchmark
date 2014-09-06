a104aff1501907e6a347a1c42e81c7b4add17325
nogueira <nogueira@rhrk.uni-kl.de>
2014-09-06 19:06:19 +0200

    Adding configuration file for easy deployment

---
+++ BenchmarkTool/benchmarktool/models/0_conf.py
@@ -0,0 +1,16 @@
+# coding: utf8
+"""
+Application static configuration
+"""
+
+"Database connection"
+DB_USERNAME = '<username>'
+DB_PASS = '<password>'
+DB_ADDRESS = '<domain or IP>'
+DB_NAME = '<database name>'
+
+" Site config "
+HOST_URL = 'http://<dns or ip>:8000'
+APPLICATION = "<application_name>/"
+
+BENCHMARK_SCRIPT = "<full path and name of benchmark script (ex. /usr/local/share/pyheston/GetArgs.py)>"
