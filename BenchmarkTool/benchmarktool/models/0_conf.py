ec371e5ed2751a9da96adc160f2409cdd7c2e5ac
nogueira <nogueira@rhrk.uni-kl.de>
2014-09-06 19:50:30 +0200

    Clean Package

--- BenchmarkTool/benchmarktool/models/0_conf.py
+++ BenchmarkTool/benchmarktool/models/0_conf.py
@@ -10,7 +10,7 @@ DB_ADDRESS = '<domain or IP>'
 DB_NAME = '<database name>'
 
 " Site config "
-HOST_URL = 'http://<dns or ip>:8000'
+HOST_URL = 'http://<dns or ip>:8000/'
 APPLICATION = "<application_name>/"
 
 BENCHMARK_SCRIPT = "<full path and name of benchmark script (ex. /usr/local/share/pyheston/GetArgs.py)>"
