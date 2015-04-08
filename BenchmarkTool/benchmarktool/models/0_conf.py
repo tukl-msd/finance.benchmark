0714662fd5caa822bdfdb3330bee4378364add9a
cpnogueira <carolpnog@gmail.com>
2015-04-08 14:50:20 +0200

    Adding parameters to FPGA simulation

--- BenchmarkTool/benchmarktool/models/0_conf.py
+++ BenchmarkTool/benchmarktool/models/0_conf.py
@@ -14,6 +14,9 @@ HOST_URL = 'http://<dns or ip>:8000/'
 APPLICATION = "<application_name>/"
 
 BENCHMARK_SCRIPT = "<full path and name of benchmark script (ex. /usr/local/share/pyheston/GetArgs.py)>"
+FPGA_BENCHMARK_PATH = "<full path of the benchmark scripts for FPGA>"
+FPGA_BENCHMARK_SCRIPT_ML = "<name of the multilevel script for FPGA simulations>"
+FPGA_BENCHMARK_SCRIPT_SL = "<name of the singlelevel script for FPGA simulations>"
 
 " E-mail configuration"
 M_USERNAME = '<username>'
